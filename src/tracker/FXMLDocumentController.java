e44893bab1bf553169bbbadd0befb99edb788d63
obideuce <obideuce@precision>
2019-01-26 12:17:14 -0500

    created Product class working tableview demo

--- src/tracker/FXMLDocumentController.java
+++ src/tracker/FXMLDocumentController.java
@@ -6,44 +6,49 @@ package tracker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FXMLDocumentController implements Initializable {
     
-    private Label label;
-    @FXML
-    private Button invBtnAddToInventory;
-    @FXML
-    private TableColumn<?, ?> invColProdUID;
-    @FXML
-    private TableColumn<?, ?> invColProdName;
-    @FXML
-    private TableColumn<?, ?> invColProdCost;
-    @FXML
-    private TableColumn<?, ?> invColProdPrice;
-    @FXML
-    private TableColumn<?, ?> invColProdQty;
-    @FXML
-    private Button invBtnDelete;
-    @FXML
-    private Button invBtnModify;
-    @FXML
-    private Button invBtnModify1;
+    @FXML private Button invBtnAddToInventory, invBtnModify, invBtnDelete, invBtnFilters;
     
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    @FXML private TableView<Product> invTable;
+    @FXML private TableColumn<Product, Integer> invColProdUID;
+    @FXML private TableColumn<Product, String> invColProdName;
+    @FXML private TableColumn<Product, Double> invColProdCost;
+    @FXML private TableColumn<Product, Double> invColProdPrice;
+    @FXML private TableColumn<Product, Integer> invColProdQty;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // prepare columns in table
+        invColProdUID.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        invColProdName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        invColProdCost.setCellValueFactory(new PropertyValueFactory<>("cost"));
+        invColProdPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        invColProdQty.setCellValueFactory(new PropertyValueFactory<>("qtyOnHand"));
+        
+        // get products for table
+        invTable.setItems(getProducts());
     }    
     
+    private void handleButtonAction(ActionEvent event) {
+        //todo
+    }
+    
+    public ObservableList<Product> getProducts() {
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product(1001,"test product name", 3.5, 9.49, 125));
+        
+        return products;
+    }
 }
---
+++ src/tracker/Product.java
@@ -0,0 +1,67 @@
+/*
+ * Here comes the text of your license
+ * Each line should be prefixed with  * 
+ */
+package tracker;
+
+import java.util.Random;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Product {
+    private SimpleIntegerProperty Id, qtyOnHand;
+    private SimpleStringProperty name;
+    private SimpleDoubleProperty cost, price;
+
+    public Product(int id, String name, double cost, double price, int qtyOnHand) {
+        Id = new SimpleIntegerProperty(id);
+        this.qtyOnHand = new SimpleIntegerProperty(qtyOnHand);
+        this.name = new SimpleStringProperty(name);
+        this.cost = new SimpleDoubleProperty(cost);
+        this.price = new SimpleDoubleProperty(price);
+    }   
+
+    public Integer getId() {
+        return Id.get();
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public Double getCost() {
+        return cost.get();
+    }
+
+    public Double getPrice() {
+        return price.get();
+    }
+    
+    public Integer getQtyOnHand() {
+        return qtyOnHand.get();
+    }
+
+    public void setId(SimpleIntegerProperty Id) {
+        this.Id = Id;
+    }
+   
+    public void setName(SimpleStringProperty name) {
+        this.name = name;
+    }
+
+    
+    public void setCost(SimpleDoubleProperty cost) {
+        this.cost = cost;
+    }
+
+    public void setPrice(SimpleDoubleProperty price) {
+        this.price = price;
+    }
+    
+    public void setQtyOnHand(SimpleIntegerProperty qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }  
+    
+    
+}
